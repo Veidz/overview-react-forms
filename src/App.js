040368e7e8c124f08e6e10291ed91267a39a4dc4
Joao Victor Veidz <joaovictorveidz@gmail.com>
2021-11-05 22:23:01 -0300

    create base form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="container">
-      
+      <h1>Login</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+  render() {
+    return (
+      <form>
+        <div className="form-control">
+          <label htmlFor="email">
+            Email
+            <input 
+              type="email" 
+              id="email" 
+              name="email" 
+              required 
+            />
+          </label>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="password">
+            Password
+            <input type="password" id="password" name="password" required />
+          </label>
+        </div>
+
+        <div className="form-control">
+          <textarea 
+            name="textarea"
+            value=""
+          />
+        </div>
+
+        <div className="form-checkbox">
+          <label htmlFor="signed">
+            Keep Signed In
+            <input 
+              type="checkbox"
+              name="signed"
+            />
+          </label>
+        </div>
+
+        <button className="btn">Login</button>
+      </form>
+    )
+  }
+}
